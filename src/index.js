98a06d098cb69792765b3911a7da08288fb3ceda
ctsdm <156813428+CTSDM@users.noreply.github.com>
2024-07-11 18:39:14 +0200

    Fix typo
    There was a missing fixing in a typo that lead to a variable not being
    correctly referentiated

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import './style.css'
 import startGame from './domManipulation'
 const GAMEBOARD_SIZE = 10;
 const NUMBER_OF_SHIPS = 4;
-startGame(GAMEBOARD_SIZE, NUMBER_OF_CHIPS);
+startGame(GAMEBOARD_SIZE, NUMBER_OF_SHIPS);
