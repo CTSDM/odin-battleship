f5d1e9359c0d96241a8a9d1b4b9bc17bff578a6a
ctsdm <156813428+CTSDM@users.noreply.github.com>
2024-07-11 18:35:46 +0200

    Correct typo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css'
 import startGame from './domManipulation'
 const GAMEBOARD_SIZE = 10;
-const NUMBER_OF_CHIPS = 4;
+const NUMBER_OF_SHIPS = 4;
 startGame(GAMEBOARD_SIZE, NUMBER_OF_CHIPS);
