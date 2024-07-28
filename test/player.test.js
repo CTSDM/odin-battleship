eb13d663337537c0ccef405fef69b8899df60d14
CTSDM <156813428+CTSDM@users.noreply.github.com>
2024-07-28 16:46:48 +0200

    Fix buggy condition for test

--- test/player.test.js
+++ test/player.test.js
@@ -1,18 +1,19 @@
 import Gameboard from '../src/gameboard';
 import Player from '../src/player'
 
+const size = 10;
 test('Player has a gameboard', () => {
-    const player = new Player();
+    const player = new Player(size);
     expect(player.gameboard instanceof Gameboard).toBe(true);
 });
 
 test('Player is human', () => {
-    const player = new Player();
+    const player = new Player(size);
     expect(player.human).toBe(true);
 });
 
 test('Player is not human', () => {
-    const player = new Player(false);
+    const player = new Player(size, false);
     expect(player.human).toBe(false);
 });
 
