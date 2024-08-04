f83e371ada1f01c79bffe242969347b77a17d4f8
CTSDM <156813428+CTSDM@users.noreply.github.com>
2024-08-04 16:20:04 +0200

    Fix bug where computer move was not optimal

--- src/calculationFunctions.js
+++ src/calculationFunctions.js
@@ -149,13 +149,13 @@ export function computerPlays(size, computerPlayRecord, isRandom) {
             return computerPlaysRandom(size);
     }
     else {
-        if (computerPlayRecord.lastHitIndex === -1)
-            return computerPlaysRandom(size);
         const newCords = getNearbyCoordinates(size, computerPlayRecord);
-        if (newCords === -1)
-            return computerPlaysRandom(size);
-        else
+        if (newCords !== -1)
             return newCords;
+        else if (computerPlayRecord.nonSunkShipsHitPosition.length > 0)
+            return getNearbyCoordinatesOld(size, computerPlayRecord);
+        else
+            return computerPlaysRandom(size);
     }
 }
 
